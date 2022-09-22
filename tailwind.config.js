35e8f82657599e7e07d454899f2753dba2b90d3b
ChocBloc <85860569+ChocBloc@users.noreply.github.com>
2022-09-23 09:19:14 +1000

    progress

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,9 @@
 module.exports = {
   content: [
     "./index.html",
+    "./00-Template/index.html",
     "./01-ExpandingCards/index.html",
     "./02-ProgressSteps/index.html",
-    "./00-Template/index.html",
   ],
   theme: {
     extend: {},
