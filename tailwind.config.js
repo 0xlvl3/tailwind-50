fd3118c1d5560f2c9290022b62a931b6673a8229
ChocBloc <85860569+ChocBloc@users.noreply.github.com>
2022-09-22 16:17:05 +1000

    refactoring 50 with tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./01-ExpandingCards/index.html",
+    "./02-ProgressSteps/index.html",
+    "./00-Template/index.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
